e2a1a865ff1b0f6d5901a35d999d3c0ee047e9e3
Steve Kostecke <steve@kostecke.net>
2026-01-09 21:37:20 -0600

    Started adding meetings 1 year in advance with January 2027

--- bin/add-rrra-meetings.sh
+++ bin/add-rrra-meetings.sh
@@ -8,26 +8,40 @@
 #
 # Dependencies: ?
 
-# We must be in the root of our hugo source tree
-YEAR=`date +%Y`
-NEXTYEAR=`echo $YEAR 1 + p | dc`
-MYPATH="content/calendar/$NEXTYEAR"
-[ -d $MYPATH ] || \
-	(echo "Not at root of web source tree."; exit 1)
-
+# User configuration
 CLUB_START="19:00"
 ARES_START="18:30"
+CLUB_CONTACT="president"
+ACTIVITY_CONTACT="president"
+ARES_CONTACT="KC0ODE"
 LOCATION="[West Fargo FD HQ](/places/west-fargo-fire-department-headquarters/)"
+BASE="content/calendar"
+
+YEAR=`date +%Y`
+MONTH=`date +%m`
+MONTH_NAME=`date -d "$MONTH/1/2001" +"%B"`
+LC_MONTH=`echo $MONTH_NAME | tr [:upper:] [:lower:]`
+NEXTYEAR=`echo $YEAR 1 + p | dc`
+MYPATH="$BASE/$NEXTYEAR"
+
+# Find the 3rd Tuesday of the target month
+DAY=`cal $MONTH $NEXTYEAR | awk 'NF>4 { print $3 }' | sed -n '4p'`
+
+# Sanity check & prep
+[ -d $BASE ] || \
+	(echo "Not at root of web source tree."; exit 1)
+[ -d "$MYPATH" ] || mkdir "$MYPATH"
 
 # Uses variables assembled elsewhere
 build_club_meeting() {
 cat << DONE > $FILENAME
 +++
-contact = "president"
+contact = "$CLUB_CONTACT"
 date = "$EVENTDATE"
 publishDate = "$PUBDATE"
 location = "$LOCATION"
-title = "$MON_NAME Business Meeting"
+title = "$MONTH_NAME Business Meeting"
+slug = "club-meeting"
 dates = [ "Club Meetings" ]
 outputs = [ "HTML", "Calendar" ]
 update = 0
@@ -42,11 +56,12 @@ DONE
 build_ares_meeting() {
 cat << DONE > $FILENAME
 +++
-contact = "KC0ODE"
+contact = "$ARES_CONTACT"
 date = "$EVENTDATE"
 publishDate = "$PUBDATE"
 location = "$LOCATION"
-title = "$MON_NAME ARES Meeting"
+title = "$MONTH_NAME ARES Meeting"
+slug = "ares-meeting"
 dates = [ "ARES Meetings" ]
 outputs = [ "HTML", "Calendar" ]
 update = 0
@@ -58,11 +73,12 @@ DONE
 build_activity() {
 cat << DONE > $FILENAME
 +++
-contact = "president"
+contact = "$ACTIVITY_CONTACT"
 date = "$EVENTDATE"
 publishDate = "$PUBDATE"
 location = "$LOCATION"
-title = "$MON_NAME Activity: TBD"
+title = "$MONTH_NAME Activity: TBD"
+slug = "club-activity"
 dates = [ "Club Meetings" ]
 outputs = [ "HTML", "Calendar" ]
 update = 0
@@ -71,52 +87,50 @@ aliases = [ "" ]
 DONE
 }
 
-# Business & ARES Meetings
-MONTHS="2 5 8 11"
-for M in $MONTHS;
-do
-
-	MON_NAME=`date -d "$M/1/2001" +"%B"`
-	MONTH=`echo $MON_NAME | sed 's/\([[:alpha:]]\)/\L\1/'`
-
-	# Find the 3rd Tuesday of each month
-	D=`cal $M $NEXTYEAR | awk 'NF>4 { print $3 }' | sed -n '4p'`
+meeting_month() {
 
 	# Skip months with existing entries
-	FILENAME="$MYPATH/$MONTH-meeting.md"
+	FILENAME="$MYPATH/$LC_MONTH-meeting.md"
 	[ -f "$FILENAME" ] || {
-		EVENTDATE=`date -d "$CLUB_START $M/$D/$NEXTYEAR" --iso-8601=seconds`
-		PUBDATE=`date -d "$EVENTDATE -120 days" +"%Y-%m-%d"`
+		EVENTDATE=`date -d "$CLUB_START $MONTH/$DAY/$NEXTYEAR" --iso-8601=seconds`
+		PUBDATE=`date +"%Y-%m-%d"`
 		build_club_meeting
 	}
 
-	FILENAME="$MYPATH/$MONTH-ares-meeting.md"
+	FILENAME="$MYPATH/$LC_MONTH-ares-meeting.md"
 	[ -f "$FILENAME" ] || {
-		EVENTDATE=`date -d "$ARES_START $M/$D/$NEXTYEAR" --iso-8601=seconds`
-		PUBDATE=`date -d "$EVENTDATE -120 days" +"%Y-%m-%d"`
+		EVENTDATE=`date -d "$ARES_START $MONTH/$DAY/$NEXTYEAR" --iso-8601=seconds`
+		PUBDATE=`date +"%Y-%m-%d"`
 		build_ares_meeting
 	}
 
-done
+}
 
-# Activity Meetings
-MONTHS="1 3 4 6 7 9 10"
-for M in $MONTHS;
-do
-
-	MON_NAME=`date -d "$M/1/2001" +"%B"`
-	MONTH=`echo $MON_NAME | sed 's/\([[:alpha:]]\)/\L\1/'`
-
-	# Find the 3rd Tuesday of each month
-	D=`cal $M $NEXTYEAR | awk 'NF>4 { print $3 }' | sed -n '4p'`
+activity_month() {
 
 	# Skip months with existing entries
-	FILENAME="$MYPATH/$MONTH-meeting.md"
+	FILENAME="$MYPATH/$LC_MONTH-meeting.md"
 	[ -f "$FILENAME" ] || {
-		EVENTDATE=`date -d "$CLUB_START $M/$D/$NEXTYEAR" --iso-8601=seconds`
-		PUBDATE=`date -d "$EVENTDATE -120 days" +"%Y-%m-%d"`
+		EVENTDATE=`date -d "$CLUB_START $MONTH/$DAY/$NEXTYEAR" --iso-8601=seconds`
+		PUBDATE=`date +"%Y-%m-%d"`
 		build_activity
 	}
 
-done
+}
+
+# Main code
+
+case $MONTH in
+
+02 | 05 | 08 | 11)
+	# Business & ARES Meetings
+	meeting_month
+	;;
+
+01 | 03 | 04 | 06 | 07 | 09 | 10)
+	# Activity Meetings
+	activity_month
+	;;
+
+esac
 
---
+++ content/calendar/2027/january-meeting.md
@@ -0,0 +1,12 @@
++++
+contact = "president"
+date = "2027-01-19T19:00:00-06:00"
+publishDate = "2026-01-09"
+location = "[West Fargo FD HQ](/places/west-fargo-fire-department-headquarters/)"
+title = "January Activity: TBD"
+slug = "club-activity"
+dates = [ "Club Meetings" ]
+outputs = [ "HTML", "Calendar" ]
+update = 0
+aliases = [ "" ]
++++
