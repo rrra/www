453f40bff127b709b3943f56dfd63642ef81bf3e
Steve Kostecke <steve@kostecke.net>
2026-02-15 15:51:19 -0600

    Added content and updated a helper script
    Parse an RSS feed rather than scrape HTML to find a podcast for a blog post.
    During this rewrite I switched from perl to awk in a shell script.

--- bin/add-rrra-meetings.sh
+++ bin/add-rrra-meetings.sh
@@ -6,9 +6,12 @@
 #
 # Purpose: Generate calendar entries for monthly meetings
 #
-# Dependencies: ?
+# Dependencies: awk, cal, date, dc, sed, tr
 
+###########################################################################
 # User configuration
+###########################################################################
+
 CLUB_START="19:00"
 ARES_START="18:30"
 CLUB_CONTACT="president"
@@ -17,22 +20,29 @@ ARES_CONTACT="KC0ODE"
 LOCATION="[West Fargo FD HQ](/places/west-fargo-fire-department-headquarters/)"
 BASE="content/calendar"
 
-YEAR=`date +%Y`
-MONTH=`date +%m`
-MONTH_NAME=`date -d "$MONTH/1/2001" +"%B"`
-LC_MONTH=`echo $MONTH_NAME | tr [:upper:] [:lower:]`
-NEXTYEAR=`echo $YEAR 1 + p | dc`
+###########################################################################
+# Values we need
+###########################################################################
+
+YEAR=`date +%Y`                                      # 4 digit year
+NEXTYEAR=`echo $YEAR 1 + p | dc`                     # next year
+MONTH=`date +%m`                                     # e.g. 01
+MONTH_NAME=`date -d "$MONTH/1/2001" +"%B"`           # e.g. January
+LC_MONTH=`echo $MONTH_NAME | tr [:upper:] [:lower:]` # lower case month
+                                                     # for the filename
+# Publish the meeting immediately
+PUBDATE=`date +"%Y-%m-%d"`
+
+# Target directory
 MYPATH="$BASE/$NEXTYEAR"
 
-# Find the 3rd Tuesday of the target month
+# Our meetings are on the 3rd Tuesday of the target month
 DAY=`cal $MONTH $NEXTYEAR | awk 'NF>4 { print $3 }' | sed -n '4p'`
 
-# Sanity check & prep
-[ -d $BASE ] || \
-	(echo "Not at root of web source tree."; exit 1)
-[ -d "$MYPATH" ] || mkdir "$MYPATH"
+###########################################################################
+# Subroutines
+###########################################################################
 
-# Uses variables assembled elsewhere
 build_club_meeting() {
 cat << DONE > $FILENAME
 +++
@@ -78,7 +88,7 @@ date = "$EVENTDATE"
 publishDate = "$PUBDATE"
 location = "$LOCATION"
 title = "$MONTH_NAME Activity: TBD"
-slug = "club-activity"
+slug = "club-meeting"
 dates = [ "Club Meetings" ]
 outputs = [ "HTML", "Calendar" ]
 update = 0
@@ -89,18 +99,15 @@ DONE
 
 meeting_month() {
 
-	# Skip months with existing entries
 	FILENAME="$MYPATH/$LC_MONTH-meeting.md"
 	[ -f "$FILENAME" ] || {
 		EVENTDATE=`date -d "$CLUB_START $MONTH/$DAY/$NEXTYEAR" --iso-8601=seconds`
-		PUBDATE=`date +"%Y-%m-%d"`
 		build_club_meeting
 	}
 
 	FILENAME="$MYPATH/$LC_MONTH-ares-meeting.md"
 	[ -f "$FILENAME" ] || {
 		EVENTDATE=`date -d "$ARES_START $MONTH/$DAY/$NEXTYEAR" --iso-8601=seconds`
-		PUBDATE=`date +"%Y-%m-%d"`
 		build_ares_meeting
 	}
 
@@ -108,18 +115,26 @@ meeting_month() {
 
 activity_month() {
 
-	# Skip months with existing entries
 	FILENAME="$MYPATH/$LC_MONTH-meeting.md"
 	[ -f "$FILENAME" ] || {
 		EVENTDATE=`date -d "$CLUB_START $MONTH/$DAY/$NEXTYEAR" --iso-8601=seconds`
-		PUBDATE=`date +"%Y-%m-%d"`
 		build_activity
 	}
 
 }
 
+###########################################################################
 # Main code
+###########################################################################
 
+# Make sure we're in the right starting place
+[ -d $BASE ] || \
+	(echo "Not at root of web source tree."; exit 1)
+
+# Ensure that the target directory esists
+[ -d "$MYPATH" ] || mkdir "$MYPATH"
+
+# Add a meeting next year for this month
 case $MONTH in
 
 02 | 05 | 08 | 11)
---
+++ content/post/2026/repeater-basics.md
@@ -0,0 +1,19 @@
++++
+authors = [ "K0STK" ]
+categories = [ "Education" ]
+slug = ""
+tags = [ "ARRL", "podcast" ]
+moreText = "Listen To The Podcast"
+thumbnail = "https://assets.blubrry.com/coverart/300/930449-721631.jpg"
+date = "2026-02-15T15:46:33-06:00"
+title = "Repeater Basics"
+description = "Repeater Basics: The On the Air podcast goes back to basics in the February 2026 episode, when QST Technical Editor Conrad Trautmann, N2YCH, joins the podcast to discuss what repeaters are, how they work, and what you need in order to access your local repeater. "
++++
+In this episode of ARRL&apos;s monthly On The Air podcast: The On the Air podcast goes back to basics in the February 2026 episode, when QST Technical Editor Conrad Trautmann, N2YCH, joins the podcast to discuss what repeaters are, how they work, and what you need in order to access your local repeater.  [^1]
+
+[^1]: Repeater Basics, blubrry podcasting, retrieved February 15 2026, https://blubrry.com/arrlontheair/152208346/repeater-basics/
+<!--more-->
+
+{{< audio "https://media.blubrry.com/arrlontheair/content.blubrry.com/arrlontheair/Audio.mp3" "Download the podcast" >}}
+
+{{< about-on-the-air >}}
